63e61a80d9f32adf3c28bdd363274497404b1a0c
Jeremy-R-J <61316636+Jeremy-R-J@users.noreply.github.com>
2021-03-16 13:08:23 -0700

    push up of web fundamental algos

---
+++ WebFundamentalsWork/Algo/Loop Algos.js
@@ -0,0 +1,17 @@
+//for (var i = 1; i < 20; i++) {
+//if (i % 2 == 1)
+//     console.log(i)
+//}
+
+
+//sum and print 1-5
+
+
+
+var sum = 0
+for (i = 1; i < 6; i++) {
+    console.log('num' + i);
+    sum += i;
+    console.log('sum' + sum)
+
+}
---
+++ WebFundamentalsWork/Algo/array_algo.js
@@ -0,0 +1,20 @@
+//var testArr = [6, 3, 5, 1, 2, 4]
+//var sum = 0
+//for (i = 0; i < testArr.length; i++) {
+//  sum += testArr[i];
+//   console.log('num' + testArr[i]);
+//   console.log(('sum' + sum))
+//}
+
+//Boom I got it.!  Need to get things on same line though will be asking about this again
+
+
+var testArr = [6, 3, 5, 1, 2, 4]
+
+for (i = 0; i < testArr.length; i++) {
+
+    newArr = testArr[i] * i
+
+    console.log(newArr)
+}
+//Anser is right need to learn to console.log as array
