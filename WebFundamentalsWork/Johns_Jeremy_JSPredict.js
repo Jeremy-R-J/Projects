4fe7c30d75ed3fe267e60edd1881a01fa28a88f0
Jeremy-R-J <61316636+Jeremy-R-J@users.noreply.github.com>
2021-03-29 15:08:26 -0700

    WebFundamentalWork

---
+++ WebFundamentalsWork/Johns_Jeremy_JSPredict.js
@@ -0,0 +1,18 @@
+function myBirthYearFunc(){
+        console.log("I was born in" + 1980);
+//undefined would be printed to your console!
+
+
+function myBirthYearFunc(birthYearInput){
+        console.log("I was born in" + birthYearInput);
+    }//undefined would be printed to your console!
+
+
+    function add(num1, num2){
+            console.log("Summing Numbers!");
+            console.log("num1 is: " + num1);
+            console.log("num2 is: " + num2);
+            var sum = num1 + num2;
+            console.log(sum);
+        }//30
+        
