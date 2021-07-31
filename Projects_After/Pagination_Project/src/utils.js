a3f0120f8436267e1b80a63f3c32e915eff11005
Jeremy-R-J <61316636+Jeremy-R-J@users.noreply.github.com>
2021-07-31 13:51:36 -0700

    Pagination Project Complete
    Display an array on a page with access to more arrays on separate pages.

--- Projects_After/Pagination_Project/src/utils.js
+++ Projects_After/Pagination_Project/src/utils.js
@@ -1,3 +1,12 @@
-const paginate = () => {}
+const paginate = (followers) => {
+  const itemsPerPage = 12;
+  const pages = Math.ceil(followers.length / itemsPerPage);
+  //   the following creates an array of arrays
+  const newFollowers = Array.from({ length: pages }, (_, index) => {
+    const start = index * itemsPerPage;
+    return followers.slice(start, start + itemsPerPage);
+  });
+  return newFollowers;
+};
 
-export default paginate
+export default paginate;
