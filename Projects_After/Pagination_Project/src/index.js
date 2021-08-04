887225a4e30b80940cad84fc9488c297cd05ebc8
Jeremy-R-J <61316636+Jeremy-R-J@users.noreply.github.com>
2021-08-03 22:19:32 -0700

    Update index.js
    small start

--- Projects_After/Pagination_Project/src/index.js
+++ Projects_After/Pagination_Project/src/index.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
 ReactDOM.render(
+  //getting started
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
