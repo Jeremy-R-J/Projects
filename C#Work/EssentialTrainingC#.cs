ecb365a7b9b787f09f7d704c4ad3bbd71aae47d7
Jeremy-R-J <61316636+Jeremy-R-J@users.noreply.github.com>
2021-09-07 12:18:31 -0700

    C# beginning work
    Just getting syntax and things down.  Debugging and Logging practice.

---
+++ C#Work/EssentialTrainingC#.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace EssenTrainingApp
+{
+    class Program
+    {
+        public static List<string> Words;
+        static void Main(string[] args)
+        {
+            Words = new List<string>();
+            Words.Add("Bread");
+            Words.Add("Milk");
+            Words.Add("Cheese");
+            CrazyMathProblem();
+            ReadTextFile();
+        }
+        private static void ReadTextFile()
+        {
+            try
+            {
+                using(var sr = new StreamReader(@"C:\temp\test.txt"))
+                {
+                    string contents = sr.ReadToEnd();
+                    Console.WriteLine(contents);
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Could not file the file.");
+            }
+        }
+        private static int CrazyMathProblem()
+        {
+            var income = 1000;
+            for(var i = 10; i > 0; i--)
+            {
+                income = income = (income / i);
+            }
+                return income;
+        }
+    }
+}
